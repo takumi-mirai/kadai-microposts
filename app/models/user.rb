e5843d17a73a7d0535ac3c15f7bd6569ef7a1c5b
takumi.mirai <artistechnology8@gmail.com>
2021-01-18 03:56:37 +0000

    favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favorites
+  has_many :favoritings, through: :favorites, source: :micropost
   
   def follow(other_user)
     unless self == other_user
@@ -29,5 +31,18 @@ class User < ApplicationRecord
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  def favorite(other_micropost)
+    self.favorites.find_or_create_by(micropost_id: other_micropost.id)
+  end
+  
+  def unfavorite(other_micropost)
+    favorite_item = self.favorites.find_by(micropost_id: other_micropost.id)
+    favorite_item.destroy if favorite_item
+  end
+  
+  def favorite?(other_micropost)
+    self.favoritings.include?(other_micropost)
   end
 end
